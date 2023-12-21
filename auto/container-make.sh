61dec7d6c3628872f958fbccd80b4497951c6d16
ToastXC <100072983+toastxc@users.noreply.github.com>
2023-12-21 11:18:52 +0800

    addded multi container engine support

--- auto/container-make.sh
+++ auto/container-make.sh
@@ -1,14 +1,19 @@
+# I personally prefer podman but this script will work for either platform
+echo "set the container engine to Docker if that's what you use, podman is default"
+engine="podman"
 # kill old
-podman container stop autoguard
-podman container rm autoguard
-podman rmi autoguard
-# create new
+$engine container stop autoguard
+$engine container rm autoguard
+$engine rmi autoguard
+# decreased time for deployment
 cargo clean
-podman build -t autoguard .
-podman run  \
+# create new
+$engine build -t autoguard .
+$engine run  \
   --name "autoguard" \
   --env-file .env \
 autoguard .
+echo "completed"
 
 
 
